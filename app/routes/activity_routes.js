bca2dc72fed589f4216a3e7c12dc1034e74b8d0e
nancyho629 <nancyho629@gmail.com>
2019-10-12 15:25:54 -0400

    Create routes for activity and trip
    -Created routes for activity and trip

---
+++ app/routes/activity_routes.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const passport = require('passport')
+const Activity = require('../models/activity')
+const customErrors = require('../../lib/custom_errors')
+const handle404 = customErrors.handle404
+const requireOwnership = customErrors.requireOwnership
+const removeBlanks = require('../../lib/remove_blank_fields')
+const requireToken = passport.authenticate('bearer', { session: false })
+const router = express.Router()
+
+// INDEX
+router.get('/activity', requireToken, (req, res, next) => {
+  Activity.find({ activity: req.activity.id })
+    .then(activities => {
+      activities.map(activity => activity.toObject())
+      return activities.reverse()
+    })
+    .then(activities => res.status(200).json({ activities: activities }))
+    .catch(next)
+})
+
+// CREATE
+router.post('/activities', requireToken, (req, res, next) => {
+  req.body.activity.owner = req.user.id
+
+  Activity.create(req.body.activity)
+    .then(activity => {
+      res.status(201).json({ activity: activity.toOBject() })
+    })
+    .catch(next)
+})
+
+// PATCH
+router.patch('/activities/:id', requireToken, removeBlanks, (req, res, next) => {
+  delete req.body.activity.owner
+  delete req.body.activity.trip
+
+  Activity.findById(req.params.id)
+    .then(handle404)
+    .then(activity => {
+      requireOwnership(req, activity)
+      return activity.updateOne(req.body.activity)
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+// SHOW
+router.get('/activities/:id', requireToken, (req, res, next) => {
+  Activity.findById(req.params.id)
+    .then(handle404)
+    .then(activity => {
+      requireOwnership(req, activity)
+    })
+    .then(activity => res.status(200).json({ activity: activity.toObject() }))
+    .catch(next)
+})
+
+// DELETE
+router.delete('/activities/:id', requireToken, (req, res, next) => {
+  Activity.findById(req.params.id)
+    .then(handle404)
+    .then(activity => {
+      requireOwnership(req, activity)
+      activity.deleteOne()
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
---
+++ app/routes/trip_routes.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const passport = require('passport')
+const Trip = require('../models/trip')
+const customErrors = require('../../lib/custom_errors')
+const handle404 = customErrors.handle404
+const requireOwnership = customErrors.requireOwnership
+const removeBlanks = require('../../lib/remove_blank_fields')
+const requireToken = passport.authenticate('bearer', { session: false })
+const router = express.Router()
+const Activity = require('../models/activity')
+
+// INDEX
+router.get('/trips', requireToken, (req, res, next) => {
+  Trip.find({ owner: req.user.id })
+    .then(trips => {
+      trips.map(trip => trip.toObject())
+      return trips.reverse()
+    })
+    .then(trips => res.status(200).json({ trips: trips }))
+    .catch(next)
+})
+
+// CREATE
+router.post('/trips', requireToken, (req, res, next) => {
+  req.body.item.owner = req.user.id
+
+  Trip.create(req.body.trip)
+    .then(trip => {
+      res.status(201).json({ trip: trip.toObject() })
+    })
+    .catch(next)
+})
+
+// PATCH
+router.patch('/trips/:id', requireToken, removeBlanks, (req, res, next) => {
+  delete req.body.trip.owner
+
+  Trip.findById(req.params.id)
+    .then(handle404)
+    .then(trip => {
+      requireOwnership(req, trip)
+      return trip.updateOne(req.body.trip)
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+// SHOW
+router.get('/trips/:id', requireToken, (req, res, next) => {
+  Trip.findbyId(req.params.id)
+    .then(handle404)
+    .then(trip => {
+      requireOwnership(req, trip)
+    })
+    .then(trip => res.status(200).json({ trip: trip.toObject() }))
+    .catch(next)
+})
+
+// DELETE
+router.delete('/trips/:id', requireToken, (req, res, next) => {
+  Trip.findById(req.params.id)
+    .then(handle404)
+    .then(trip => {
+      requireOwnership(req, trip)
+      const id = trip
+      Activity.deleteMany({ trip: id }, (err, res) => {
+        if (err) throw err
+      })
+      Trip.remove()
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+module.exports = router
